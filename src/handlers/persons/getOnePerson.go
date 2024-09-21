fcde9d0a9325a78f53403b98d5253e130a2fe42c
saullo.reis <saulloreis01@hotmail.com>
2024-09-20 23:39:42 -0300

    adding func to encapsulate response

--- src/handlers/persons/getOnePerson.go
+++ src/handlers/persons/getOnePerson.go
@@ -8,6 +8,44 @@ import (
 	database "lacosv2.com/src/database/config"
 )
 
+func creatingResponse(person SearchFieldsPerson) Response{
+	responseResponsible := ResponseResponsiblePerson{
+		IDPerson: person.ResponsiblePerson.IDPerson.Int64,
+		IDResponsible: person.ResponsiblePerson.IDResponsible.Int64,
+		Name: person.ResponsiblePerson.Name.String,
+		Relationship: person.ResponsiblePerson.Relationship.String,
+		RG: person.ResponsiblePerson.RG.String,
+		CPF: person.ResponsiblePerson.CPF.String,
+		CellPhone: person.ResponsiblePerson.CellPhone.String,
+	}
+
+	responsePerson := Response{
+		IDPerson: person.IDPerson.Int64,
+		Name: person.Name.String,
+		BirthDate: person.BirthDate.String,
+		RG: person.RG.String,
+		CPF: person.CPF.String,
+		CadUnico: person.CadUnico.String,
+		NIS: person.NIS.String,
+		School: person.School.String,
+		Address: person.Address.String,
+		AddressNumber: person.AddressNumber.String,
+		BloodType: person.BloodType.String,
+		Neighborhood: person.Neighborhood.String,
+		City: person.City.String,
+		CEP: person.CEP.String,
+		HomePhone: person.HomePhone.String,
+		CellPhone: person.CellPhone.String,
+		ContactPhone: person.ContactPhone.String,
+		Email: person.Email.String,
+		CurrentAge: person.CurrentAge.Int64,
+		Active: person.Active.String,
+		ResponsiblePerson: responseResponsible,
+	}
+
+	return responsePerson
+}
+
 func GetOnePerson(c * gin.Context){
 	idUser := c.Param("idUser")
 	db, err := database.ConnectDB()
@@ -19,8 +57,11 @@ func GetOnePerson(c * gin.Context){
 		return
 	} 
 
+	query := "SELECT person.id_person, person.name, person.birth_date, person.rg, person.cpf, person.cad_unico, person.nis, person.school, person.address, person.address_number, person.blood_type, person.neighborhood, person.city, person.cep, person.home_phone, person.cell_phone, person.contact_phone, person.email, person.current_age, person.active, rperson.id_responsible, rperson.name as rname, rperson.relationship, rperson.rg as rrg, rperson.cpf as rcpf, rperson.cell_phone as rcell_phone FROM persons person LEFT JOIN responsible_person rperson ON person.id_person = rperson.id_person WHERE person.id_person = $1"
+	
 	var searchPerson SearchFieldsPerson
-	err = db.QueryRow("SELECT person.id_person, person.name, person.birth_date, person.rg, person.cpf, person.cad_unico, person.nis, person.school, person.address, person.address_number, person.blood_type, person.neighborhood, person.city, person.cep, person.home_phone, person.cell_phone, person.contact_phone, person.email, person.current_age, person.active, rperson.id_responsible, rperson.name as rname, rperson.relationship, rperson.rg as rrg, rperson.cpf as rcpf, rperson.cell_phone as rcell_phone FROM persons person LEFT JOIN responsible_person rperson ON person.id_person = rperson.id_person WHERE person.id_person = $1", idUser).Scan(&searchPerson.IDPerson, &searchPerson.Name, &searchPerson.BirthDate, &searchPerson.RG, &searchPerson.CPF, &searchPerson.CadUnico, &searchPerson.NIS, &searchPerson.School, &searchPerson.Address, &searchPerson.AddressNumber, &searchPerson.BloodType, &searchPerson.Neighborhood, &searchPerson.City, &searchPerson.CEP, &searchPerson.HomePhone , &searchPerson.CellPhone, &searchPerson.ContactPhone, &searchPerson.Email, &searchPerson.CurrentAge, &searchPerson.Active, &searchPerson.ResponsiblePerson.IDResponsible, &searchPerson.ResponsiblePerson.Name, &searchPerson.ResponsiblePerson.Relationship, &searchPerson.ResponsiblePerson.RG, &searchPerson.ResponsiblePerson.CPF, &searchPerson.ResponsiblePerson.CellPhone)
+	err = db.QueryRow(query, idUser).Scan(&searchPerson.IDPerson, &searchPerson.Name, &searchPerson.BirthDate, &searchPerson.RG, &searchPerson.CPF, &searchPerson.CadUnico, &searchPerson.NIS, &searchPerson.School, &searchPerson.Address, &searchPerson.AddressNumber, &searchPerson.BloodType, &searchPerson.Neighborhood, &searchPerson.City, &searchPerson.CEP, &searchPerson.HomePhone , &searchPerson.CellPhone, &searchPerson.ContactPhone, &searchPerson.Email, &searchPerson.CurrentAge, &searchPerson.Active, &searchPerson.ResponsiblePerson.IDResponsible, &searchPerson.ResponsiblePerson.Name, &searchPerson.ResponsiblePerson.Relationship, &searchPerson.ResponsiblePerson.RG, &searchPerson.ResponsiblePerson.CPF, &searchPerson.ResponsiblePerson.CellPhone)
+
 	if err != nil {
 		if err == sql.ErrNoRows {
 			c.JSON(http.StatusBadRequest, gin.H{
@@ -36,40 +77,8 @@ func GetOnePerson(c * gin.Context){
 		return
 	}
 
-	responseResponsible := ResponseResponsiblePerson{
-		IDPerson: searchPerson.ResponsiblePerson.IDPerson.Int64,
-		IDResponsible: searchPerson.ResponsiblePerson.IDResponsible.Int64,
-		Name: searchPerson.ResponsiblePerson.Name.String,
-		Relationship: searchPerson.ResponsiblePerson.Relationship.String,
-		RG: searchPerson.ResponsiblePerson.RG.String,
-		CPF: searchPerson.ResponsiblePerson.CPF.String,
-		CellPhone: searchPerson.ResponsiblePerson.CellPhone.String,
-	}
+	responsePerson := creatingResponse(searchPerson)
 
-	responsePerson := Response{
-		IDPerson: searchPerson.IDPerson.Int64,
-		Name: searchPerson.Name.String,
-		BirthDate: searchPerson.BirthDate.String,
-		RG: searchPerson.RG.String,
-		CPF: searchPerson.CPF.String,
-		CadUnico: searchPerson.CadUnico.String,
-		NIS: searchPerson.NIS.String,
-		School: searchPerson.School.String,
-		Address: searchPerson.Address.String,
-		AddressNumber: searchPerson.AddressNumber.String,
-		BloodType: searchPerson.BloodType.String,
-		Neighborhood: searchPerson.Neighborhood.String,
-		City: searchPerson.City.String,
-		CEP: searchPerson.CEP.String,
-		HomePhone: searchPerson.HomePhone.String,
-		CellPhone: searchPerson.CellPhone.String,
-		ContactPhone: searchPerson.ContactPhone.String,
-		Email: searchPerson.Email.String,
-		CurrentAge: searchPerson.CurrentAge.Int64,
-		Active: searchPerson.Active.String,
-		ResponsiblePerson: responseResponsible,
-	}
-	
 	c.JSON(http.StatusOK, gin.H{
 		"status": 200,
 		"message": "Sucesso",
